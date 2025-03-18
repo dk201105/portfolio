c9ce1c6bdb060c3ec424dbefdd52a8483aa92cd6
dk201105 <dkjee2023@gmail.com>
2025-03-18 19:08:34 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set the public directory for serving static files
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html as the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Set up the server to listen on port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
