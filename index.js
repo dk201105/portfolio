c860f226b299a08fa8d3cf2ebd2cd4804d961598
dk201105 <dkjee2023@gmail.com>
2025-03-19 06:07:24 +0530

    3rd Commit

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Set the public directory for serving static files
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'project-root/public')));
 
-// Serve index.html as the main page
+// Serve CSS files from 'public/css'
+app.use('/css', express.static(path.join(__dirname, 'project-root/public/css')));
+
+// Serve images
+app.use('/images', express.static(path.join(__dirname, 'project-root/public/images')));
+
+// Serve external assets
+app.use('/external', express.static(path.join(__dirname, 'project-root/public/external')));
+
+// Serve the main page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'main.html'));
+    res.sendFile(path.join(__dirname, 'project-root/public', 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, "/images")));
-
-
-// Set up the server to listen on port 3000
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
